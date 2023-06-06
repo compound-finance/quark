03553881509e6c85dff7435588d5cbbeac6862cb
Geoffrey Hayes <hayesgm@gmail.com>
2023-06-06 00:26:03 -0700

    Bump to alpha4, use correct bytecode

--- client/src/Quark.ts
+++ client/src/Quark.ts
@@ -9,17 +9,19 @@ export { Command, prepare } from './Command';
 export { buildSol, buildYul } from './Compiler';
 import { getNetwork, relayers } from './Relayer';
 import { keccak256 } from '@ethersproject/keccak256';
+export * as Script from './Script';
+import { deployedBytecode as relayerBytecode } from '../../out/Relayer.sol/Relayer.json'
 
 export const UINT256_MAX = new Uint256("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 
-const v1code = '0x60008080808080335af115606d5760173d81810192605f601d856070565b938383863e60405180156066575b601281602060059301604052602060c682395195600660a9893960506001820189608a565b8088019360af8539019101608a565b5533600155f35b506060602b565b80fd5b906040519182156082575b8201604052565b60609250607b565b9060ff60039180601d1a600185015380601e1a60028501531691015356fe62000000565bfe5b600254620000005760016002556005565b600054ff';
+const bytecodeV1 = relayerBytecode.object;
 
 const initCodes: { [version: number]: { [network: string]: string } } = {
   1: {
-    'goerli': v1code,
-    'optimism-goerli': v1code,
-    'arbitrum-goerli': v1code,
-    'arbitrum': v1code,
+    'goerli': bytecodeV1,
+    'optimism-goerli': bytecodeV1,
+    'arbitrum-goerli': bytecodeV1,
+    'arbitrum': bytecodeV1,
   }
 };
 
