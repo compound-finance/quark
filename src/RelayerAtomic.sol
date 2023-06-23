a1e3849cc6f6a77052c3cd3c805f5e6ade67340a
Geoffrey Hayes <hayesgm@gmail.com>
2023-06-22 21:10:42 -0700

    Add RelayerAtomic, the one that isn't metamorphic

---
+++ src/RelayerAtomic.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "./Relayer.sol";
+import "./QuarkScript.sol";
+import "./CodeJar.sol";
+
+contract RelayerAtomic is Relayer {
+    CodeJar public immutable codeJar;
+
+    error QuarkAlreadyActive(address quark);
+    error QuarkNotActive(address quark);
+    error QuarkInvalid(address quark, bytes32 isQuarkScriptHash);
+    error QuarkInitFailed(address quark, bool create2Failed);
+    error QuarkCallFailed(address quark, bytes error);
+    error QuarkAddressMismatch(address expected, address created);
+
+    mapping(address => address) public quarkCodes;
+
+    constructor() {
+        address codeJar_;
+        if (msg.data.length > 0) {
+            codeJar_ = abi.decode(msg.data, (address));
+        }
+
+        if (codeJar_ == address(0)) {
+            codeJar_ = address(new CodeJar());
+        }
+        codeJar = CodeJar(codeJar_);
+    }
+
+    /**
+     * @notice Helper function to return a quark address for a given account.
+     */
+    function getQuarkAddress(address account) public override view returns (address) {
+        return address(uint160(uint(
+            keccak256(
+                abi.encodePacked(
+                    bytes1(0xff),
+                    address(this),
+                    uint256(0),
+                    keccak256(
+                        abi.encodePacked(
+                            getQuarkInitCode(),
+                            abi.encode(account)
+                        )
+                    )
+                )
+            )))
+        );
+    }
+
+    /**
+     * @notice The init code for a Quark wallet.
+     * @dev The actual init code for a Quark wallet, passed to `create2`. This is
+     *      the yul output from `./Quark.yul`, but it's impossible to reference
+     *      a yul object in Solidity, so we do a two phase compile where we
+     *      build that code, take the outputed bytecode and paste it in here.
+     */
+    function getQuarkInitCode() public override pure returns (bytes memory) {
+        return hex"6040604f806065600039338152602081016020601f193801823951337f46ce4d9fc828e2af4f167362c7c43e310c76adc313cd8fe11e785726f972b4f6557f3bb5ebf00f3b539fbe3d28370e5631dd2bb9520dffcea6daf564f94582db811155016000f3fe6000604038603f1901823960208160048180805163665f107960e01b82525af11560405780808051368280378136915af43d82803e15603c573d90f35b3d90fd5b633c5bb1a760e21b8152600490fd";
+    }
+
+    // Saves quark code for an quark address into storage. This is required
+    // since we can't pass unique quark code in the `create2` constructor,
+    // since it would end up at a different wallet address.
+    function saveQuark(address quarkAddress, bytes memory quarkCode) internal {
+        quarkCodes[quarkAddress] = codeJar.saveCode(quarkCode);
+    }
+
+    /**
+     * @notice Returns the code associated with a running quark for `msg.sender`
+     * @dev This is generally expected to be used only by the Quark wallet itself
+     *      in the constructor phase to get its code.
+     */
+    function readQuarkCodeAddress() external view returns (address) {
+        address quarkAddress = msg.sender;
+        address quarkCodeAddress = quarkCodes[quarkAddress];
+        if (quarkCodeAddress == address(0)) {
+            revert QuarkNotActive(quarkAddress);
+        }
+        return quarkCodeAddress;
+    }
+
+    // Clears quark data a) to save gas costs, and b) so another quark can
+    // be run for the same quarkAddress in the future.
+    function clearQuark(address quarkAddress) internal {
+        quarkCodes[quarkAddress] = address(0);
+    }
+
+    // Internal function for running a quark. This handles the `create2`, invoking the script,
+    // and then calling `destruct` to clean it up. We attempt to revert on any failed step.
+    function _runQuark(address account, bytes memory quarkCode, bytes memory quarkCalldata) internal override returns (bytes memory) {
+        address quarkAddress = getQuarkAddress(account);
+
+        // Ensure a quark isn't already running
+        if (quarkCodes[quarkAddress] != address(0)) {
+            revert QuarkAlreadyActive(quarkAddress);
+        }
+
+        // Stores the quark in storage so it can be loaded via `readQuark` in the `create2`
+        // constructor code (see `./Quark.yul`).
+        saveQuark(quarkAddress, quarkCode);
+
+        // Appends the account to the init code (the argument). This is meant to be part
+        // of the `create2` init code, so that we get a unique quark wallet per address.
+        bytes memory initCode = abi.encodePacked(
+            getQuarkInitCode(),
+            abi.encode(account)
+        );
+
+        uint256 initCodeLen = initCode.length;
+
+        uint256 existingQuarkSize;
+
+        assembly {
+            existingQuarkSize := extcodesize(quarkAddress)
+        }
+
+        address quark;
+        if (existingQuarkSize > 0) {
+            quark = quarkAddress;
+        } else {
+            // The call to `create2` that creates the (temporary) quark wallet.
+
+            assembly {
+                quark := create2(0, add(initCode, 32), initCodeLen, 0)
+            }
+
+            // Ensure that the wallet was created.
+            if (uint160(address(quark)) == 0) {
+                revert QuarkInitFailed(quarkAddress, true);
+            }
+
+            if (quarkAddress != address(quark)) {
+                revert QuarkAddressMismatch(quarkAddress, address(quark));
+            }
+        }
+
+        // Call into the new quark wallet with a (potentially empty) message to hit the fallback function.
+        (bool callSuccess, bytes memory res) = address(quark).call{value: msg.value}(quarkCalldata);
+        if (!callSuccess) {
+            revert QuarkCallFailed(quarkAddress, res);
+        }
+
+        clearQuark(quarkAddress);
+
+        // We return the result from the call, but it's not particularly important.
+        return res;
+    }
+}
---
+++ test/RelayerAtomic.t.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "./lib/YulHelper.sol";
+import "./lib/Counter.sol";
+import "./lib/CounterScript.sol";
+
+import "../src/Relayer.sol";
+import "../src/RelayerAtomic.sol";
+
+contract QuarkTest is Test {
+    event Ping(uint256 value);
+
+    Relayer public relayer;
+    Counter public counter;
+
+    uint256 internal accountPrivateKey;
+    uint256 internal searcherPrivateKey;
+
+    address internal account;
+    address internal searcher;
+
+    constructor() {
+        relayer = new RelayerAtomic();
+        console.log("Relayer deployed to: %s", address(relayer));
+
+        counter = new Counter();
+        counter.setNumber(0);
+        console.log("Counter deployed to: %s", address(counter));
+    }
+
+    function setUp() public {
+        // nothing
+    }
+
+    function testAtomicPing() public {
+        bytes memory ping = new YulHelper().get("Ping.yul/Logger.json");
+
+        // TODO: Check who emitted.
+        vm.expectEmit(false, false, false, true);
+        emit Ping(55);
+
+        bytes memory data = relayer.runQuark(ping);
+        assertEq(data, abi.encode());
+    }
+
+    function testAtomicIncrementer() public {
+        bytes memory incrementer = new YulHelper().get("Incrementer.yul/Incrementer.json");
+
+        assertEq(counter.number(), 0);
+
+        vm.prank(address(0xaa));
+        bytes memory data = relayer.runQuark(incrementer);
+        assertEq(data, abi.encode());
+        assertEq(counter.number(), 3);
+
+        uint256 gl = gasleft();
+        relayer.runQuark(incrementer);
+        uint256 gasUsed = gl - gasleft();
+        assertEq(counter.number(), 3);
+        assertEq(gasUsed, 0);
+    }
+
+    function testAtomicGetOwner() public {
+        bytes memory getOwner = new YulHelper().get("GetOwner.yul/GetOwner.json");
+
+        vm.prank(address(0xaa));
+        bytes memory data = relayer.runQuark(getOwner);
+        assertEq(data, abi.encode(55));
+    }
+
+    function testAtomicCallback() public {
+        bytes memory callback = new YulHelper().get("Callback.yul/Callback.json");
+
+        assertEq(counter.number(), 0);
+
+        vm.prank(address(0xaa));
+        bytes memory data = relayer.runQuark(callback);
+        assertEq(data, abi.encode());
+        assertEq(counter.number(), 11);
+    }
+
+    function testAtomicNoCallbacks() public {
+        bytes memory noCallback = new YulHelper().get("NoCallback.yul/Callback.json");
+
+        assertEq(counter.number(), 0);
+
+        vm.prank(address(0xaa));
+        bytes memory data = relayer.runQuark(noCallback);
+        assertEq(data, abi.encode());
+        assertEq(counter.number(), 0);
+    }
+
+    function testAtomicCounterScript() public {
+        bytes memory counterScript = new YulHelper().getDeployed("CounterScript.sol/CounterScript.json");
+
+        assertEq(counter.number(), 0);
+
+        vm.prank(address(0xaa));
+        bytes memory data = relayer.runQuark(counterScript, abi.encodeCall(CounterScript.run, (counter)));
+        assertEq(data, abi.encode());
+        assertEq(counter.number(), 2);
+    }
+
+    function testAtomicDirectIncrementer() public {
+        bytes memory incrementer = new YulHelper().get("Incrementer.yul/Incrementer.json");
+
+        // assertEq(incrementer, QuarkInterface(quark).virtualCode81());
+        // assertEq(address(0x6c022704D948c71930B35B6F6bb725bc8d687E7F), QuarkInterface(quark).quarkAddress25(address(1)));
+
+        assertEq(counter.number(), 0);
+
+        vm.prank(address(0xaa));
+        (bool success0, bytes memory data0) = address(relayer).call(incrementer);
+        assertEq(success0, true);
+        assertEq(data0, abi.encode());
+        assertEq(counter.number(), 3);
+    }
+}
