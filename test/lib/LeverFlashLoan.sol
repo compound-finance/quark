da0abb05f56698cb78b1d56aefe46146b54fdd14
banky <banky@compound.finance>
2023-09-27 12:42:52 -0400

    Add LeverFlashLoan file

---
+++ test/lib/LeverFlashLoan.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "../../src/QuarkScript.sol";
+
+contract LeverFlashLoan is QuarkScript {
+    function run(Comet comet) external returns (bytes memory) {}
+}
