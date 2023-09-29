0a66c52dba990e15534fc9e84ce5e0ad1d05c231
Scott Silver <scott@compound.finance>
2023-09-29 11:17:58 -0400

    ccipReceive, nonce management

--- src/QuarkBroker.sol
+++ src/QuarkBroker.sol
@@ -9,6 +9,10 @@ import "./QuarkWallet.sol";
  * a factory for new QuarkWallets at deterministic addresses.
  */
 contract QuarkBroker {
+    /// @notice Bit-packed nonce values
+    /// chainId => userAdress => nonce bucket => nonce
+    mapping(uint256 => mapping(address => mapping(uint256 => uint256))) public nonces;
+
     CodeJar public codeJar;
 
     /**
@@ -21,7 +25,7 @@ contract QuarkBroker {
     /**
      * @notice create a QuarkWallet at a deterministic address based upon the CodeJar and the underlying owner account address.
      */
-    function create(address account) public returns (address) {
+    function createWallet(address account) public returns (address) {
         return address(new QuarkWallet{salt: 0}(account, codeJar));
     }
 
@@ -35,7 +39,55 @@ contract QuarkBroker {
         bytes32 r,
         bytes32 s
     ) public payable returns (bytes memory) {
-        address wallet = create(account);
+        uint256 walletCodeLen;
+        address walletAddress = walletAddressForAccount(account);
+
+        assembly {
+            walletCodeLen := extcodesize(walletAddress)
+        }
+        if (walletCodeLen == 0) {
+            createWallet(account);
+        }
+
         return QuarkWallet(wallet).executeQuarkOperation(op, v, r, s);
+    }
+
+    /**
+     * @notice Helper function to return a quark address for a given account.
+     */
+    function walletAddressForAccount(address account) external view returns (address) {
+        return address(uint160(uint(
+            keccak256(
+                abi.encodePacked(
+                    bytes1(0xff),
+                    address(this),
+                    uint256(0),
+                    keccak256(
+                        abi.encodePacked(
+                            type(QuarkWallet).creationCode,
+                            abi.encode(account),
+                            abi.encode(address(codeJar))
+                        )
+                    )
+                )
+            )))
+        );
+    }
+
+    /**
+     * @notice Helper function to return a quark address for a given account.
+     */
+    function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) public {
+        (chainId, userAddress, nonce) = abi.decode(any2EvmMessage.data, (uint256, address, uint256));
+        _updateNonce(chainId, userAddress, nonce);
+    }
+
+    /**
+     * @notice Helper function to return a quark address for a given account.
+     */
+    function _updateNonce(chainId, userAddress, nonce) internal {
+        uint256 bucket = nonce >> 8;
+        uint256 mask = 1 << (nonce & 0xff);
+        nonces[chainId][userAddress][bucket] |= mask;
     }
 }
