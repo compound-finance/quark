9d46f43a2995ce52c36f8b26f5da41ffb01b9265
fluffywaffles <skorlir@gmail.com>
2023-10-05 12:54:22 -0700

    refactor: remove relayer, owner from QuarkScript
    - relayer() is no longer a thing
    - owner() is already an immutable of the wallet, and since there is no
      longer going to be a TUP, we can access this directly without loading
      from storage -- better anyway, because you can no longer write to it

--- src/QuarkScript.sol
+++ src/QuarkScript.sol
@@ -1,25 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import "./Relayer.sol";
-
 contract QuarkScript {
-    function owner() internal view returns (address payable) {
-        address payable owner_;
-        assembly {
-          owner_ := sload(0x3bb5ebf00f3b539fbe3d28370e5631dd2bb9520dffcea6daf564f94582db8111) // keccak("org.quark.owner")
-        }
-        return owner_;
-    }
-
-    function relayer() internal view returns (Relayer) {
-        Relayer relayer_;
-        assembly {
-          relayer_ := sload(0x46ce4d9fc828e2af4f167362c7c43e310c76adc313cd8fe11e785726f972b4f6) // keccak("org.quark.relayer")
-        }
-        return relayer_;
-    }
-
     function sloadU256(string memory key) internal view returns (uint256) {
         return uint256(sload(key));
     }
@@ -43,10 +25,4 @@ contract QuarkScript {
             sstore(key, value)
         }
     }
-
-    modifier onlyRelayer() {
-        require(msg.sender == address(relayer()));
-        _;
-    }
 }
-
