838b9cfa3585e83643a72b3033e5cac034ee50df
Hans Wang <2709448+cwang25@users.noreply.github.com>
2023-10-18 16:24:46 -0700

    Update test/core_scripts/UniswapFlashSwapMultiCall.t.sol
    Co-authored-by: scott-silver <silversh@gmail.com>

--- test/core_scripts/UniswapFlashSwapMultiCall.t.sol
+++ test/core_scripts/UniswapFlashSwapMultiCall.t.sol
@@ -41,7 +41,7 @@ contract UniswapFlashSwapMultiCallTest is Test {
         // Set up some funds for test
         deal(WETH, address(wallet), 10 ether);
 
-        // User has establish position in comet, try to use flash swap to leverage up
+        // User has a borrow position in comet and tries to use a flash swap to leverage up
         // Borrow 1 ETH worth of USDC from comet, and purchase 1 ETH re-supply and remaining USDC back to Comet
         // Some computation is required to get the right number to pass into UniswapFlashSwapMultiCall core scripts
 
