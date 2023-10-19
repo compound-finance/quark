76ecac7b4cf56114756a13f24132aefa576fbecc
Hans Wang <hans@compound.finance>
2023-10-19 14:45:08 -0700

    ethcall test

--- test/core_scripts/Ethcall.t.sol
+++ test/core_scripts/Ethcall.t.sol
@@ -4,121 +4,150 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "forge-std/StdUtils.sol";
+import "forge-std/StdMath.sol";
+import "forge-std/interfaces/IERC20.sol";
 
-import "./../../src/CodeJar.sol";
 import "./../../src/QuarkWallet.sol";
-import "./../../src/core_scripts/Ethcall.sol";
+import "./../../src/QuarkWalletFactory.sol";
+import "./../../src/core_scripts/EthCall.sol";
 import "./../lib/YulHelper.sol";
+import "./../lib/SignatureHelper.sol";
 import "./../lib/Counter.sol";
+import "./custom_scripts/SupplyComet.sol";
+import "./interfaces/IComet.sol";
 
-contract EthcallTest is Test {
-    CodeJar public codeJar;
-    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    bytes32 internal constant QUARK_OPERATION_TYPEHASH =
-        keccak256(
-            "QuarkOperation(bytes scriptSource,bytes scriptCalldata,uint256 nonce,uint256 expiry)"
-        );
+contract EthCallTest is Test {
+    QuarkWalletFactory public factory;
     Counter public counter;
     // Need alice info here, for signature to QuarkWallet
-    address alice = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    uint256 alicePK = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+    uint256 alicePK = 0xa11ce;
+    address alice = vm.addr(alicePK);
+    // Contracts address on mainnet
+    address constant comet = 0xc3d688B66703497DAA19211EEdff47f25384cdc3;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    SignatureHelper public signatureHelper;
+
     function setUp() public {
-        codeJar = new CodeJar();
-        console.log("CodeJar deployed to: %s", address(codeJar));
-
-        codeJar.saveCode(
-            new YulHelper().getDeployed(
-                "Ethcall.sol/Ethcall.json"
-            )
-        );
-
         counter = new Counter();
         counter.setNumber(0);
-        console.log("Counter deployed to : %s", address(counter));
+        signatureHelper = new SignatureHelper();
+        factory = new QuarkWalletFactory();
     }
 
     // Test Case #1: Invoke Counter contract via signature
-    function testEthCallCounterBySig() public {
-        QuarkWallet wallet = new QuarkWallet{salt: 0}(alice, codeJar);
+    function testEthCallCounter() public {
+        QuarkWallet wallet = QuarkWallet(factory.create(alice, 0));
         bytes memory ethcall = new YulHelper().getDeployed(
-            "Ethcall.sol/Ethcall.json"
+            "EthCall.sol/EthCall.json"
         );
 
-        
         QuarkWallet.QuarkOperation memory op = QuarkWallet.QuarkOperation({
             scriptSource: ethcall,
             scriptCalldata: abi.encodeWithSelector(
-                Ethcall.run.selector,
-                address(counter),
-                hex"",
-                abi.encodeWithSignature(
-                    "increment(uint256)",
-                    (1)
+                EthCall.run.selector, address(counter), abi.encodeCall(Counter.incrementBy, (1)), 0
                 ),
-                0
-            ),
-            nonce: 0,
+            nonce: wallet.nextUnusedNonce(),
             expiry: type(uint256).max,
-            admitCallback: true
+            allowCallback: true
         });
 
         assertEq(counter.number(), 0);
-        (uint8 v, bytes32 r, bytes32 s) = aliceSignature(wallet, op);
-        bytes memory result = wallet.executeQuarkOperation(op, v, r, s);
+        (uint8 v, bytes32 r, bytes32 s) = signatureHelper.signOp(wallet, op, alicePK);
+        wallet.executeQuarkOperation(op, v, r, s);
         assertEq(counter.number(), 1);
     }
 
-    // Test Case #2: Invoke Counter contract
-    function testEthCallCounter() public {
-        QuarkWallet wallet = new QuarkWallet{salt: 0}(alice, codeJar);
+    // Test Case #2: Supply USDC to Comet
+    function testEthCallSupplyUSDCToComet() public {
+        QuarkWallet wallet = QuarkWallet(factory.create(alice, 0));
         bytes memory ethcall = new YulHelper().getDeployed(
-            "Ethcall.sol/Ethcall.json"
+            "EthCall.sol/EthCall.json"
         );
 
-        assertEq(counter.number(), 0);
-        bytes memory result = wallet.executeQuarkOperation(
-            ethcall,
-            abi.encodeWithSelector(
-                Ethcall.run.selector,
-                address(counter),
-                hex"",
-                abi.encodeWithSignature(
-                    "increment(uint256)",
-                    (1)
+        // Set up some funds for test
+        deal(USDC, address(wallet), 1000e6);
+        // Approve Comet to spend USDC
+        QuarkWallet.QuarkOperation memory op = QuarkWallet.QuarkOperation({
+            scriptSource: ethcall,
+            scriptCalldata: abi.encodeWithSelector(
+                EthCall.run.selector, address(USDC), abi.encodeCall(IERC20.approve, (comet, 1000e6)), 0
                 ),
-                0
-            )
-        );
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (uint8 v, bytes32 r, bytes32 s) = signatureHelper.signOp(wallet, op, alicePK);
+        wallet.executeQuarkOperation(op, v, r, s);
 
-        assertEq(counter.number(), 1);
+        assertEq(IComet(comet).balanceOf(address(wallet)), 0);
+        // Supply Comet
+        QuarkWallet.QuarkOperation memory op2 = QuarkWallet.QuarkOperation({
+            scriptSource: ethcall,
+            scriptCalldata: abi.encodeWithSelector(
+                EthCall.run.selector, address(comet), abi.encodeCall(IComet.supply, (USDC, 1000e6)), 0
+                ),
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (uint8 v2, bytes32 r2, bytes32 s2) = signatureHelper.signOp(wallet, op2, alicePK);
+        wallet.executeQuarkOperation(op2, v2, r2, s2);
+
+        // Since there is rouding diff, assert on diff is less than 10 wei
+        assertLt(stdMath.delta(1000e6, IComet(comet).balanceOf(address(wallet))), 2);
     }
 
-    // Test Case #3: Supply USDC to Comet
-
-    // Test Case #4: Withdraw USDC from Comet
-
-    
-    function aliceSignature(
-        QuarkWallet wallet,
-        QuarkWallet.QuarkOperation memory op
-    ) internal view returns (uint8, bytes32, bytes32) {
-        bytes32 structHash = keccak256(
-            abi.encode(
-                QUARK_OPERATION_TYPEHASH,
-                op.scriptSource,
-                op.scriptCalldata,
-                op.nonce,
-                op.expiry
-            )
+    // Test Case #3: Withdraw USDC from Comet
+    function testEthCallWithdrawUSDCFromComet() public {
+        QuarkWallet wallet = QuarkWallet(factory.create(alice, 0));
+        bytes memory ethcall = new YulHelper().getDeployed(
+            "EthCall.sol/EthCall.json"
         );
-        bytes32 digest = keccak256(
-            abi.encodePacked("\x19\x01", wallet.DOMAIN_SEPARATOR(), structHash)
-        );
-        return
-            vm.sign(
-                // ALICE PRIVATE KEY
-                0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80,
-                digest
-            );
+
+        // Set up some funds for test
+        deal(WETH, address(wallet), 100 ether);
+
+        // Approve Comet to spend WETH
+        QuarkWallet.QuarkOperation memory op = QuarkWallet.QuarkOperation({
+            scriptSource: ethcall,
+            scriptCalldata: abi.encodeWithSelector(
+                EthCall.run.selector, address(WETH), abi.encodeCall(IERC20.approve, (comet, 100 ether)), 0
+                ),
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (uint8 v, bytes32 r, bytes32 s) = signatureHelper.signOp(wallet, op, alicePK);
+        wallet.executeQuarkOperation(op, v, r, s);
+
+        // Supply WETH to Comet
+        op = QuarkWallet.QuarkOperation({
+            scriptSource: ethcall,
+            scriptCalldata: abi.encodeWithSelector(
+                EthCall.run.selector, address(comet), abi.encodeCall(IComet.supply, (WETH, 100 ether)), 0
+                ),
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (v, r, s) = signatureHelper.signOp(wallet, op, alicePK);
+        wallet.executeQuarkOperation(op, v, r, s);
+
+        // Withdraw USDC from Comet
+        op = QuarkWallet.QuarkOperation({
+            scriptSource: ethcall,
+            scriptCalldata: abi.encodeWithSelector(
+                EthCall.run.selector, address(comet), abi.encodeCall(IComet.withdraw, (USDC, 1000e6)), 0
+                ),
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (v, r, s) = signatureHelper.signOp(wallet, op, alicePK);
+        wallet.executeQuarkOperation(op, v, r, s);
+
+        assertEq(IERC20(USDC).balanceOf(address(wallet)), 1000e6);
     }
 }
---
+++ test/core_scripts/interfaces/IComet.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+interface IComet {
+    function getAssetInfo(uint8 i) external view returns (AssetInfo memory);
+
+    function baseToken() external view returns (address);
+
+    function supply(address asset, uint amount) external;
+
+    function withdraw(address asset, uint amount) external;
+
+    function balanceOf(address owner) external view returns (uint256);
+
+    function getPrice(address priceFeed) external view returns (uint256);
+
+    function baseTokenPriceFeed() external view returns (address);
+
+    function borrowBalanceOf(address account) external view returns (uint256);
+
+    function collateralBalanceOf(address account, address asset) external view returns (uint128);
+
+    function getAssetInfoByAddress(address asset) external view returns (AssetInfo memory);
+
+    function supplyTo(address dst, address asset, uint amount) external;
+
+    function baseScale() external view returns (uint);
+
+    function isLiquidatable(address account) external view returns (bool);
+
+    function withdrawTo(address to, address asset, uint amount) external;
+
+    function withdrawFrom(address src, address to, address asset, uint amount) external;
+
+    function supplyFrom(address from, address dst, address asset, uint amount) external;
+
+    function allow(address manager, bool isAllowed_) external;
+}
+
+struct AssetInfo {
+    uint8 offset;
+    address asset;
+    address priceFeed;
+    uint64 scale;
+    uint64 borrowCollateralFactor;
+    uint64 liquidateCollateralFactor;
+    uint64 liquidationFactor;
+    uint128 supplyCap;
+}
