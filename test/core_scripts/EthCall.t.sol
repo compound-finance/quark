f7630671573f39146222b45b8ead3aa7a464183b
Hans Wang <2709448+cwang25@users.noreply.github.com>
2023-10-19 12:30:45 -0700

    Update test/core_scripts/EthCall.t.sol
    Co-authored-by: scott-silver <silversh@gmail.com>

--- test/core_scripts/EthCall.t.sol
+++ test/core_scripts/EthCall.t.sol
@@ -92,7 +92,7 @@ contract EthCallTest is Test {
             expiry: type(uint256).max,
             allowCallback: false
         });
-        (v, r, s) = signatureHelper.signOp(wallet, op2, alicePK);
+        (uint8 v2, bytes32 r2, bytes32 s2) = signatureHelper.signOp(wallet, op2, alicePK);
         wallet.executeQuarkOperation(op2, v, r, s);
 
         // Since there is rouding diff, assert on diff is less than 10 wei
