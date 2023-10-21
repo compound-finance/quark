66e567579cbe719874e1ba39f134d69eb2572382
Hans Wang <hans@compound.finance>
2023-10-20 18:12:24 -0700

    test

--- test/core_scripts/MultiCall.t.sol
+++ test/core_scripts/MultiCall.t.sol
@@ -254,4 +254,183 @@ contract MultiCallTest is Test {
         );
         wallet.executeQuarkOperation(op, v, r, s);
     }
+
+    function testConditionalChecksOnPeriodicRepay() public {
+        QuarkWallet wallet = QuarkWallet(factory.create(alice, 0));
+        bytes memory multiCall = new YulHelper().getDeployed(
+            "MultiCall.sol/MultiCall.json"
+        );
+
+        // Set up some funds for test
+        deal(WETH, address(wallet), 100 ether);
+        vm.startPrank(address(wallet));
+        IERC20(WETH).approve(comet, 100 ether);
+        IERC20(USDC).approve(comet, type(uint256).max);
+        IComet(comet).supply(WETH, 100 ether);
+        IComet(comet).withdraw(USDC, 1000e6);
+        IERC20(USDC).transfer(address(1), 1000e6); // Spent somewhere else
+        vm.stopPrank();
+
+        // Compose array of parameters
+        address[] memory callContracts = new address[](3);
+        bytes[] memory callDatas = new bytes[](3);
+        uint256[] memory callValues = new uint256[](3);
+        ConditionalChecker.CheckType[] memory checkTypes = new ConditionalChecker.CheckType[](3);
+        ConditionalChecker.Operator[] memory operators = new ConditionalChecker.Operator[](3);
+        bytes[] memory checkValues = new bytes[](3);
+
+        // Monitor wallet balance, if it ever goes over 400 USDC, it will start repaying Comet if borrowBalance is still > 0
+        // Check wallet balance of USDC
+        callContracts[0] = USDC;
+        callDatas[0] = abi.encodeCall(IERC20.balanceOf, (address(wallet)));
+        callValues[0] = 0 wei;
+        checkTypes[0] = ConditionalChecker.CheckType.Uint;
+        operators[0] = ConditionalChecker.Operator.GreaterThanOrEqual;
+        checkValues[0] = abi.encode(uint256(400e6));
+
+        // Check that wallet still has USDC borrow in Comet
+        callContracts[1] = comet;
+        callDatas[1] = abi.encodeCall(IComet.borrowBalanceOf, (address(wallet)));
+        callValues[1] = 0 wei;
+        checkTypes[1] = ConditionalChecker.CheckType.Uint;
+        operators[1] = ConditionalChecker.Operator.GreaterThan;
+        checkValues[1] = abi.encode(uint256(0));
+
+        // Supply USDC to Comet to repay
+        callContracts[2] = comet;
+        callDatas[2] = abi.encodeCall(IComet.supply, (USDC, 400e6));
+        callValues[2] = 0 wei;
+        checkTypes[2] = ConditionalChecker.CheckType.None;
+        operators[2] = ConditionalChecker.Operator.None;
+        checkValues[2] = hex"";
+
+        QuarkWallet.QuarkOperation memory op = QuarkWallet.QuarkOperation({
+            scriptSource: multiCall,
+            scriptCalldata: abi.encodeWithSelector(
+                MultiCall.runWithConditionalCheck.selector,
+                callContracts,
+                callDatas,
+                callValues,
+                checkTypes,
+                operators,
+                checkValues
+                ),
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (uint8 v, bytes32 r, bytes32 s) = signatureHelper.signOp(alicePK, wallet, op);
+        // Wallet doesn't have USDC, condition will fail
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                QuarkWallet.QuarkCallError.selector,
+                abi.encodeWithSelector(
+                    ConditionalChecker.CheckFailed.selector,
+                    abi.encode(uint256(0)),
+                    abi.encode(uint256(400e6)),
+                    ConditionalChecker.CheckType.Uint,
+                    ConditionalChecker.Operator.GreaterThanOrEqual
+                )
+            )
+        );
+        wallet.executeQuarkOperation(op, v, r, s);
+
+        // Wallet has accrue 400 USDC
+        deal(USDC, address(wallet), 400e6);
+
+        // Condition met should repay Comet
+        op = QuarkWallet.QuarkOperation({
+            scriptSource: multiCall,
+            scriptCalldata: abi.encodeWithSelector(
+                MultiCall.runWithConditionalCheck.selector,
+                callContracts,
+                callDatas,
+                callValues,
+                checkTypes,
+                operators,
+                checkValues
+                ),
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (v, r, s) = signatureHelper.signOp(alicePK, wallet, op);
+        wallet.executeQuarkOperation(op, v, r, s);
+
+        // Wallet has accrued another 400 USDC
+        deal(USDC, address(wallet), 400e6);
+        op = QuarkWallet.QuarkOperation({
+            scriptSource: multiCall,
+            scriptCalldata: abi.encodeWithSelector(
+                MultiCall.runWithConditionalCheck.selector,
+                callContracts,
+                callDatas,
+                callValues,
+                checkTypes,
+                operators,
+                checkValues
+                ),
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (v, r, s) = signatureHelper.signOp(alicePK, wallet, op);
+        wallet.executeQuarkOperation(op, v, r, s);
+
+        // Wallet has accrued another 400 USDC
+        deal(USDC, address(wallet), 400e6);
+        op = QuarkWallet.QuarkOperation({
+            scriptSource: multiCall,
+            scriptCalldata: abi.encodeWithSelector(
+                MultiCall.runWithConditionalCheck.selector,
+                callContracts,
+                callDatas,
+                callValues,
+                checkTypes,
+                operators,
+                checkValues
+                ),
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (v, r, s) = signatureHelper.signOp(alicePK, wallet, op);
+        wallet.executeQuarkOperation(op, v, r, s);
+
+        // Wallet no longer borrows from Comet, condition 2 will fail
+        deal(USDC, address(wallet), 400e6);
+
+        op = QuarkWallet.QuarkOperation({
+            scriptSource: multiCall,
+            scriptCalldata: abi.encodeWithSelector(
+                MultiCall.runWithConditionalCheck.selector,
+                callContracts,
+                callDatas,
+                callValues,
+                checkTypes,
+                operators,
+                checkValues
+                ),
+            nonce: wallet.nextUnusedNonce(),
+            expiry: type(uint256).max,
+            allowCallback: false
+        });
+        (v, r, s) = signatureHelper.signOp(alicePK, wallet, op);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                QuarkWallet.QuarkCallError.selector,
+                abi.encodeWithSelector(
+                    ConditionalChecker.CheckFailed.selector,
+                    abi.encode(uint256(0)),
+                    abi.encode(uint256(0)),
+                    ConditionalChecker.CheckType.Uint,
+                    ConditionalChecker.Operator.GreaterThan
+                )
+            )
+        );
+        wallet.executeQuarkOperation(op, v, r, s);
+
+        // Wallet fully pays off debt
+        assertEq(IComet(comet).borrowBalanceOf(address(wallet)), 0);
+    }
 }
