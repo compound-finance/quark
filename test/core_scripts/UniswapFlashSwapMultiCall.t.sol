e1eda07a3720ffd25754106fc6121e5e832f4b99
Hans Wang <hans@compound.finance>
2023-10-23 10:57:56 -0700

    misc updates for new requirements

--- test/core_scripts/UniswapFlashSwapMultiCall.t.sol
+++ test/core_scripts/UniswapFlashSwapMultiCall.t.sol
@@ -20,15 +20,19 @@ contract UniswapFlashSwapMultiCallTest is Test {
     // For signature to QuarkWallet
     uint256 alicePK = 0xa11ce;
     address alice = vm.addr(alicePK);
-    SignatureHelper public signatureHelper;
+
     // Comet address in mainnet
     address constant cometAddr = 0xc3d688B66703497DAA19211EEdff47f25384cdc3;
     address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     function setUp() public {
+        vm.createSelectFork(
+            string.concat(
+                "https://node-provider.compound.finance/ethereum-mainnet/", vm.envString("NODE_PROVIDER_BYPASS_KEY")
+            )
+        );
         factory = new QuarkWalletFactory();
-        signatureHelper = new SignatureHelper();
     }
 
     // Test #1: Using flash swap to leverage/deleverage Comet position on single asset
@@ -90,9 +94,11 @@ contract UniswapFlashSwapMultiCallTest is Test {
             scriptCalldata: abi.encodeWithSelector(UniswapFlashSwapMultiCall.run.selector, payload),
             nonce: wallet.nextUnusedNonce(),
             expiry: type(uint256).max,
-            allowCallback: true
+            allowCallback: true,
+            isReplayable: false,
+            requirements: new uint256[](0)
         });
-        (uint8 v, bytes32 r, bytes32 s) = signatureHelper.signOp(alicePK, wallet, op);
+        (uint8 v, bytes32 r, bytes32 s) = new SignatureHelper().signOp(alicePK, wallet, op);
         wallet.executeQuarkOperation(op, v, r, s);
 
         // Verify that user is now supplying 10 + 1 WETH
@@ -127,9 +133,11 @@ contract UniswapFlashSwapMultiCallTest is Test {
                 ),
             nonce: wallet.nextUnusedNonce(),
             expiry: type(uint256).max,
-            allowCallback: true
+            allowCallback: true,
+            isReplayable: false,
+            requirements: new uint256[](0)
         });
-        (uint8 v, bytes32 r, bytes32 s) = signatureHelper.signOp(alicePK, wallet, op);
+        (uint8 v, bytes32 r, bytes32 s) = new SignatureHelper().signOp(alicePK, wallet, op);
         vm.expectRevert(
             abi.encodeWithSelector(
                 QuarkWallet.QuarkCallError.selector,
@@ -182,9 +190,11 @@ contract UniswapFlashSwapMultiCallTest is Test {
             scriptCalldata: abi.encodeWithSelector(UniswapFlashSwapMultiCall.run.selector, payload),
             nonce: wallet.nextUnusedNonce(),
             expiry: type(uint256).max,
-            allowCallback: true
+            allowCallback: true,
+            isReplayable: false,
+            requirements: new uint256[](0)
         });
-        (uint8 v, bytes32 r, bytes32 s) = signatureHelper.signOp(alicePK, wallet, op);
+        (uint8 v, bytes32 r, bytes32 s) = new SignatureHelper().signOp(alicePK, wallet, op);
         vm.expectRevert(
             abi.encodeWithSelector(
                 QuarkWallet.QuarkCallError.selector,
