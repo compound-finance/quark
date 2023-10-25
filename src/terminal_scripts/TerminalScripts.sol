982532fcdf73961b9759182b7fae9f8bd760ca35
Hans Wang <hans@compound.finance>
2023-10-25 02:18:00 -0700

    adding scenario tests and terminal scripts

---
+++ src/terminal_scripts/TerminalScripts.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+contract TerminalScripts {
+    function supplyBaseToV3() external {}
+
+    function withdrawBaseFromV3() external {}
+
+    function supplyCollateralToV3() external {}
+
+    function withdrawCollateralFromV3() external {}
+
+    function buyAssetWithUSDC() external {}
+
+    function sellAssetWithUSDC() external {}
+
+    function sendToken() external {}
+
+    function sendNativeToken() external {}
+
+    function claimCOMP() external {}
+
+    function supplyMultipleCollateralAssetsToV3() external {}
+
+    function withdrawMultipleCollateralAssetsFromV3() external {}
+
+    function accountProtection() external {}
+}
