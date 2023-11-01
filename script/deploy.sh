07c323cb359f1478138363daedd39be92b0f289b
Kevin Cheng <kevincheng96@hotmail.com>
2023-11-01 11:49:20 -0700

    Create deploy script for QuarkWalletFactory (#48)
    Adds a deploy script for the `QuarkWalletFactory` and updates the README with instructions.

---
+++ script/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -exo pipefail
+
+if [ -n "$RPC_URL" ]; then
+  rpc_args="--rpc-url $RPC_URL"
+else
+  rpc_args=""
+fi
+
+if [ -n "$DEPLOYER_PK" ]; then
+  wallet_args="--private-key $DEPLOYER_PK"
+else
+  wallet_args="--unlocked"
+fi
+
+if [ -n "$ETHERSCAN_KEY" ]; then
+  etherscan_args="--verify --etherscan-api-key $ETHERSCAN_KEY"
+else
+  etherscan_args=""
+fi
+
+forge script \
+    $rpc_args \
+    $wallet_args \
+    $etherscan_args \
+    $@ \
+    script/DeployQuarkWalletFactory.s.sol:DeployQuarkWalletFactory
