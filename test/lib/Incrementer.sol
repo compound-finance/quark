98624e67d0ccae8a8c95b940017138f4e5b1b1a3
Hans Wang <hans@compound.finance>
2023-11-08 00:06:54 -0800

    Changed incrementer.sol from msg.sender to address(this)

--- test/lib/Incrementer.sol
+++ test/lib/Incrementer.sol
@@ -14,7 +14,7 @@ contract Incrementer {
 
     function incrementCounterReplayable(Counter counter) public {
         incrementCounter(counter);
-        QuarkWallet(payable(msg.sender)).stateManager().clearNonce();
+        QuarkWallet(payable(address(this))).stateManager().clearNonce();
     }
 
     fallback() external {
