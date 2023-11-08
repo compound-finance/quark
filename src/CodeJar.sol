ceb3a54aa6568f60cfc5ea96999a92c74caab596
fluffywaffles <skorlir@gmail.com>
2023-11-08 13:44:22 -0800

    refactor: remove unused custom errors from CodeJar

--- src/CodeJar.sol
+++ src/CodeJar.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.21;
 
 contract CodeJar {
-    error CodeTooLarge(uint256 sz);
-    error CodeNotFound(address codeAddress);
     error CodeInvalid(address codeAddress);
     error CodeHashMismatch(address codeAddress, bytes32 expected, bytes32 given);
 
