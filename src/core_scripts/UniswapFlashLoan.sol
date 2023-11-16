025ec6f33677c180fb36b380dfff68e9199f1758
Hans Wang <hans@compound.finance>
2023-11-16 15:22:18 -0800

    move to vendor folder

--- src/core_scripts/UniswapFlashLoan.sol
+++ src/core_scripts/UniswapFlashLoan.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-import "./lib/PoolAddress.sol";
+import "./../vendor/uniswap_v3_periphery/PoolAddress.sol";
 import "./lib/UniswapFactoryAddress.sol";
 import "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import "v3-core/contracts/interfaces/IUniswapV3Pool.sol";
