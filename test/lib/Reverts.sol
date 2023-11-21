e70e766705dbfb6c0e4b78177f60115dcff0636b
Kevin Cheng <kevincheng96@hotmail.com>
2023-11-21 15:06:25 -0800

    Test different types of reverts (#93)
    Add tests to sanity-check different revert cases:
    
    - Divide by 0
    - Revert some random bytes (we use 7)
    - Out-of-gas
    - Invalid opcode
    
    `vm.expectRevert` does not currently support matching panics (e.g. divide by 0) and `EVM Error`s (e.g. out of gas, invalid opcodes), so we use the vanilla `vm.expectRevert()` for most of these tests.

--- test/lib/Reverts.sol
+++ test/lib/Reverts.sol
@@ -1,8 +1,41 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.19;
 
+import {CodeJar} from "../../src/CodeJar.sol";
+
 contract Reverts {
     error Whoops();
+
+    function divideByZero() external {
+        uint256(100) / uint256(0);
+    }
+
+    function revertSeven() external {
+        uint256 p;
+        assembly {
+            p := mload(0x40) // free-memory pointer
+            mstore(0x40, add(p, 0x20)) // allocate 32 bytes
+            mstore(p, 0x07) // store 7 at that 32 byte space
+            revert(p, 0x20)
+        }
+    }
+
+    function outOfGas() external {
+        while (gasleft() >= 0) {}
+    }
+
+    function invalidOpcode(CodeJar codeJar) external {
+        // Deploys code that uses the INVALID (0xFE) opcode
+        bytes memory byteCode = abi.encodePacked(hex"FE");
+        address scriptAddress = codeJar.saveCode(byteCode);
+
+        (bool success, bytes memory result) = scriptAddress.call(hex"");
+        if (!success) {
+            assembly {
+                revert(add(result, 0x20), mload(result))
+            }
+        }
+    }
 
     fallback() external {
         revert Whoops();
