30c03be2072f0515a78818ed0e951c0a26212bc9
Hans Wang <2709448+cwang25@users.noreply.github.com>
2023-11-21 16:22:13 -0800

    Core Scripts - p5 - Conditional check feature (#41)
    From 🧇 : we should merge this so that we don't have to keep rebasing / merging it to keep it from growing stale
    
    As there is no MVP actions using conditions yet so:
    - this is not in-scope for the audit
    - for now, it's a proof-of-concept, we may want something more composeable / bespoke when we write conditions

---
+++ src/core_scripts/ConditionalMulticall.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.19;
+
+import "./lib/ConditionalChecker.sol";
+
+contract ConditionalMulticall {
+    error InvalidInput();
+    error MulticallError(uint256 callIndex, address callContract, bytes err);
+
+    /**
+     * @notice Execute multiple calls with conditional checks
+     * @param callContracts Array of contracts to call
+     * @param callDatas Array of encoded calldata for each call
+     * @param conditions Array of conditions specifying check types and operators
+     * @param checkValues Array of expected values for checks
+     */
+    function run(
+        address[] calldata callContracts,
+        bytes[] calldata callDatas,
+        ConditionalChecker.Condition[] calldata conditions,
+        bytes[] calldata checkValues
+    ) external returns (bytes[] memory) {
+        if (
+            callContracts.length != callDatas.length || callContracts.length != conditions.length
+                || callContracts.length != checkValues.length
+        ) {
+            revert InvalidInput();
+        }
+
+        bytes[] memory returnDatas = new bytes[](callContracts.length);
+        for (uint256 i = 0; i < callContracts.length; i++) {
+            (bool success, bytes memory returnData) = callContracts[i].delegatecall(callDatas[i]);
+            if (!success) {
+                revert MulticallError(i, callContracts[i], returnData);
+            }
+            if (
+                conditions[i].checkType != ConditionalChecker.CheckType.None
+                    && conditions[i].operator != ConditionalChecker.Operator.None
+            ) {
+                ConditionalChecker.check(abi.decode(returnData, (bytes)), checkValues[i], conditions[i]);
+            }
+
+            returnDatas[i] = abi.decode(returnData, (bytes));
+        }
+
+        return returnDatas;
+    }
+}
---
+++ src/core_scripts/lib/ConditionalChecker.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.19;
+
+library ConditionalChecker {
+    enum CheckType {
+        None,
+        Uint,
+        Int,
+        Bytes,
+        Address,
+        Bool
+    }
+
+    enum Operator {
+        None,
+        Equal,
+        NotEqual,
+        GreaterThan,
+        GreaterThanOrEqual,
+        LessThan,
+        LessThanOrEqual
+    }
+
+    struct Condition {
+        CheckType checkType;
+        Operator operator;
+    }
+
+    error CheckFailed(bytes left, bytes right, CheckType cType, Operator op);
+    error IncompatibleTypeAndOperator();
+    error InvalidOperator();
+
+    function check(bytes memory left, bytes memory right, Condition memory cond) internal pure {
+        bool result;
+        if (cond.operator == Operator.Equal) {
+            result = keccak256(left) == keccak256(right);
+        } else if (cond.operator == Operator.NotEqual) {
+            result = keccak256(left) != keccak256(right);
+        } else if (cond.operator == Operator.GreaterThan) {
+            if (cond.checkType == CheckType.Uint) {
+                result = abi.decode(left, (uint256)) > abi.decode(right, (uint256));
+            } else if (cond.checkType == CheckType.Int) {
+                result = abi.decode(left, (int256)) > abi.decode(right, (int256));
+            } else {
+                revert IncompatibleTypeAndOperator();
+            }
+        } else if (cond.operator == Operator.GreaterThanOrEqual) {
+            if (cond.checkType == CheckType.Uint) {
+                result = abi.decode(left, (uint256)) >= abi.decode(right, (uint256));
+            } else if (cond.checkType == CheckType.Int) {
+                result = abi.decode(left, (int256)) >= abi.decode(right, (int256));
+            } else {
+                revert IncompatibleTypeAndOperator();
+            }
+        } else if (cond.operator == Operator.LessThan) {
+            if (cond.checkType == CheckType.Uint) {
+                result = abi.decode(left, (uint256)) < abi.decode(right, (uint256));
+            } else if (cond.checkType == CheckType.Int) {
+                result = abi.decode(left, (int256)) < abi.decode(right, (int256));
+            } else {
+                revert IncompatibleTypeAndOperator();
+            }
+        } else if (cond.operator == Operator.LessThanOrEqual) {
+            if (cond.checkType == CheckType.Uint) {
+                result = abi.decode(left, (uint256)) <= abi.decode(right, (uint256));
+            } else if (cond.checkType == CheckType.Int) {
+                result = abi.decode(left, (int256)) <= abi.decode(right, (int256));
+            } else {
+                revert IncompatibleTypeAndOperator();
+            }
+        } else {
+            // Only reachable if check is called with an invalid operator (None:0)
+            revert InvalidOperator();
+        }
+
+        if (!result) {
+            revert CheckFailed(left, right, cond.checkType, cond.operator);
+        }
+    }
+}
