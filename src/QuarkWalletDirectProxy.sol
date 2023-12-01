d9f5782cab5c6969435e514c31c90308a96d666b
Hans Wang <hans@compound.finance>
2023-11-30 22:49:52 -0800

    trimmed down by removing OZ's dependencies, and left out receive()

--- src/QuarkWalletDirectProxy.sol
+++ src/QuarkWalletDirectProxy.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.19;
 
-import "openzeppelin/proxy/Proxy.sol";
-import {CodeJar} from "./CodeJar.sol";
 import {QuarkStateManager} from "./QuarkStateManager.sol";
 
-contract QuarkWalletDirectProxy is Proxy {
+contract QuarkWalletDirectProxy {
     error NotInitializer();
 
     bytes32 internal constant SIGNER_KEY = keccak256("signer.v1.quark");
@@ -28,7 +26,25 @@ contract QuarkWalletDirectProxy is Proxy {
         stateManager_.writeImmutable(EXECUTOR_KEY, bytes32(uint256(uint160(executor))));
     }
 
-    function _implementation() internal view virtual override returns (address) {
-        return impl;
+    fallback() external payable {
+        address implementation = impl;
+        assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch pad at memory position 0.
+            calldatacopy(0, 0, calldatasize())
+
+            // Call the implementation.
+            // out and outsize are 0 because we don't know the size yet.
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+
+            // Copy the returned data.
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
     }
 }
