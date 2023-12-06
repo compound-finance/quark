6a87134f24a822f2f92b6ac13fd06d9aa42c282b
fluffywaffles <skorlir@gmail.com>
2023-12-06 15:54:29 -0800

    refactor+bugfix: don't introduce getters yet in this refactor

--- quark-core/src/QuarkWallet.sol
+++ quark-core/src/QuarkWallet.sol
@@ -113,24 +113,6 @@ contract QuarkWallet is IERC1271 {
     }
 
     /**
-     * @dev Internal getter for the signer immutable
-     */
-    function getSigner() internal view returns (address) {
-        (bool success, bytes memory signer_) = address(this).staticcall(abi.encodeWithSignature("signer()"));
-        if (!success) revert("what are you doing");
-        return abi.decode(signer_, (address));
-    }
-
-    /**
-     * @dev Internal getter for the executor immutable
-     */
-    function getExecutor() internal view returns (address) {
-        (bool success, bytes memory executor_) = address(this).staticcall(abi.encodeWithSignature("executor()"));
-        if (!success) revert("what are you doing");
-        return abi.decode(executor_, (address));
-    }
-
-    /**
      * @notice Execute a QuarkOperation via signature
      * @dev Can only be called with signatures from the wallet's signer
      * @param op A QuarkOperation struct
@@ -168,7 +150,7 @@ contract QuarkWallet is IERC1271 {
         bytes32 digest = keccak256(abi.encodePacked("\x19\x01", getDomainSeparator(), structHash));
 
         // if the signature check does not revert, the signature is valid
-        checkValidSignatureInternal(getSigner(), digest, v, r, s);
+        checkValidSignatureInternal(signer, digest, v, r, s);
 
         // if scriptAddress not given, derive deterministic address from bytecode
         address scriptAddress = op.scriptAddress;
@@ -192,7 +174,7 @@ contract QuarkWallet is IERC1271 {
         returns (bytes memory)
     {
         // only allow the executor for the wallet to use unsigned execution
-        if (msg.sender != getExecutor()) {
+        if (msg.sender != executor) {
             revert Unauthorized();
         }
         return stateManager.setActiveNonceAndCallback(nonce, scriptAddress, scriptCalldata);
@@ -251,7 +233,7 @@ contract QuarkWallet is IERC1271 {
         // to prevent signature replayability for Quark wallets owned by the same `signer`
         bytes32 messageHash = getMessageHashForQuark(abi.encode(hash));
         // If the signature check does not revert, the signature is valid
-        checkValidSignatureInternal(getSigner(), messageHash, v, r, s);
+        checkValidSignatureInternal(signer, messageHash, v, r, s);
         return EIP_1271_MAGIC_VALUE;
     }
 
