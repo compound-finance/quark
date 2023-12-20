704b113a390a3dcd6ee089b14b7d7bfcb7cd7337
kevincheng96 <kevincheng96@hotmail.com>
2023-12-21 07:41:53 +0800

    Add gasLimit; use low-level call to prevent revert if any calls revert; use struct to avoid stack too deep

--- quark-core/src/periphery/BatchExecutor.sol
+++ quark-core/src/periphery/BatchExecutor.sol
@@ -10,39 +10,45 @@ import {QuarkWallet} from "quark-core/src/QuarkWallet.sol";
  * @author Compound Labs, Inc.
  */
 contract BatchExecutor {
-    /// @dev Thrown when the input for a function is invalid
-    error BadData();
+    /// @notice The parameters used for processing a Quark operation
+    struct OperationParams {
+        address account;
+        QuarkWallet.QuarkOperation op;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint256 gasLimit;
+    }
 
     /**
      * @notice Execute a list of QuarkOperations via signatures
-     * @param accounts List of accounts to execute operations for
-     * @param ops List of QuarkOperations
-     * @param v List of EIP-712 signature v values
-     * @param r List of EIP-712 signature r values
-     * @param s List of EIP-712 signature s values
+     * @param ops List of QuarkOperations to execute via signature
      * @return List of return values from the executed operations
      */
     function batchExecuteOperations(
-        address[] calldata accounts,
-        QuarkWallet.QuarkOperation[] calldata ops,
-        uint8[] calldata v,
-        bytes32[] calldata r,
-        bytes32[] calldata s
-    ) external returns (bytes[] memory) {
-        if (
-            accounts.length != ops.length || accounts.length != v.length || accounts.length != r.length
-                || accounts.length != s.length
-        ) {
-            revert BadData();
-        }
-
-        bytes[] memory returnData = new bytes[](accounts.length);
-        for (uint256 i = 0; i < accounts.length;) {
-            returnData[i] = QuarkWallet(payable(accounts[i])).executeQuarkOperation(ops[i], v[i], r[i], s[i]);
-            unchecked {
-                ++i;
+        OperationParams[] calldata ops
+    ) external returns (bool[] memory, bytes[] memory) {
+        bool[] memory successes = new bool[](ops.length);
+        bytes[] memory returnData = new bytes[](ops.length);
+        for (uint256 i = 0; i < ops.length;) {
+                (successes[i], returnData[i]) = executeOperation(ops[i]);
+                unchecked {
+                    ++i;
             }
         }
-        return returnData;
+        return (successes, returnData);
+    }
+
+    /// @notice Execute a single QuarkOperations via signature
+    function executeOperation(OperationParams memory op)
+        internal returns (bool, bytes memory)
+    {
+        bytes memory data = abi.encodeWithSelector(
+            QuarkWallet.executeQuarkOperation.selector,
+            op.op, op.v, op.r, op.s
+        );
+
+        (bool success, bytes memory retData) = op.account.call{gas: op.gasLimit}(data);
+        return (success, retData);
     }
 }
