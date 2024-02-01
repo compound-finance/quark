6a4eb9a99d04408a5cea7b8d99740eea4d33df60
Coburn Berry <32463466+coburncoburn@users.noreply.github.com>
2024-01-31 23:20:33 -0500

    add faucet drip script

---
+++ src/legend-scripts/src/GetDrip.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.8.23;
+
+//
+// To handle loading up new accounts on stage and dev
+interface Fauceteer {
+    function drip(address token) external;
+
+    error BalanceTooLow();
+    error RequestedTooFrequently();
+    error TransferFailed();
+}
+
+contract GetDrip {
+    /**
+     *   @notice Drip tokens from goerli faucet
+     */
+    function drip(address token) external {
+        Fauceteer(0x75442Ac771a7243433e033F3F8EaB2631e22938f).drip(token);
+    }
+}
