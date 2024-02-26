987420d2101ae394498f7b7158bae3de6a38f46b
Hans Wang <hans@compound.finance>
2024-02-26 14:20:33 -0800

    add pricefeed interface

---
+++ src/quark-core-scripts/src/Paycall.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.23;
+
+/**
+ * @title Multicall Core Script
+ * @notice Core transaction script that can be used to bundle multiple delegatecalls into a single operation
+ * @author Compound Labs, Inc.
+ */
+contract Multicall {
+    error AlreadyInitialized();
+    error InvalidCallContext();
+    error InvalidInput();
+    error MulticallError(uint256 callIndex, address callContract, bytes err);
+
+    /// @notice Storage location at which to cache this contract's address
+    bytes32 internal constant CONTRACT_ADDRESS_SLOT = keccak256("quark.scripts.multicall.address.v1");
+
+    /// @notice Storage location for the ETH price oracle address
+    bytes32 internal constant ETH_PRICE_FEED_SLOT = keccak256("quark.scripts.multicall.ethPriceFeed.v1");
+
+    constructor(address ETHPriceOracle) {
+        bytes32 slot = CONTRACT_ADDRESS_SLOT;
+        bytes32 priceFeedSlot = ETH_PRICE_FEED_SLOT;
+        assembly ("memory-safe") {
+            sstore(slot, address())
+            sstore(priceFeedSlot, ETHPriceOracle)
+        }
+    }
+
+    /**
+     * @notice Execute multiple delegatecalls to contracts in a single transaction
+     * @param callContracts Array of contracts to call
+     * @param callDatas Array of encoded calldata for each call
+     * @return Array of return data from each call
+     */
+    function run(address[] calldata callContracts, bytes[] calldata callDatas) external returns (bytes[] memory) {
+        bytes32 slot = CONTRACT_ADDRESS_SLOT;
+        address thisAddress;
+        assembly ("memory-safe") {
+            thisAddress := sload(slot)
+        }
+
+        if (address(this) == thisAddress) {
+            revert InvalidCallContext();
+        }
+        if (callContracts.length != callDatas.length) {
+            revert InvalidInput();
+        }
+
+        bytes[] memory returnDatas = new bytes[](callContracts.length);
+        for (uint256 i = 0; i < callContracts.length;) {
+            (bool success, bytes memory returnData) = callContracts[i].delegatecall(callDatas[i]);
+            if (!success) {
+                revert MulticallError(i, callContracts[i], returnData);
+            }
+            returnDatas[i] = returnData;
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        return returnDatas;
+    }
+}
