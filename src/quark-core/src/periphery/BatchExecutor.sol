77fd71e3dd8cf198a5ee2ecdf3bf7cbd4a300799
Kevin Cheng <kevincheng96@hotmail.com>
2024-03-15 13:34:12 -0700

    Add flag to `BatchExecutor` to enable or disable partial failures (#182)
    Adds an `allowPartialFailures` flag to the `batchExecuteOperations` function. This gives us the flexibility to allow partial failures on chains without Flashbots and disable partial failures on chains with Flashbots.

--- src/quark-core/src/periphery/BatchExecutor.sol
+++ src/quark-core/src/periphery/BatchExecutor.sol
@@ -10,6 +10,8 @@ import {QuarkWallet} from "quark-core/src/QuarkWallet.sol";
  * @author Compound Labs, Inc.
  */
 contract BatchExecutor {
+    error BatchExecutionError(uint256 opsIndex, bytes err);
+
     /// @notice The parameters used for processing a QuarkOperation
     struct OperationParams {
         address account;
@@ -23,20 +25,27 @@ contract BatchExecutor {
     /**
      * @notice Execute a list of QuarkOperations via signatures
      * @param ops List of QuarkOperations to execute via signature
+     * @param allowPartialFailures Whether or not to allow partial failures when any of the calls revert
      */
-    function batchExecuteOperations(OperationParams[] calldata ops) external {
-        for (uint256 i = 0; i < ops.length;) {
-            executeOperation(ops[i]);
-            unchecked {
-                ++i;
+    function batchExecuteOperations(OperationParams[] calldata ops, bool allowPartialFailures) external {
+        for (uint256 i = 0; i < ops.length; ++i) {
+            (bool success, bytes memory returnData) = executeOperation(ops[i]);
+            if (!allowPartialFailures && !success) {
+                revert BatchExecutionError(i, returnData);
             }
         }
     }
 
-    /// @notice Execute a single QuarkOperation via signature
-    function executeOperation(OperationParams memory op) internal {
+    /**
+     * @notice Execute a single QuarkOperation via signature
+     * @param op Quark Operation parameters to execute via signature
+     * @return Success and return value from the executed operation
+     */
+    function executeOperation(OperationParams memory op) internal returns (bool, bytes memory) {
         bytes memory data = abi.encodeWithSelector(QuarkWallet.executeQuarkOperation.selector, op.op, op.v, op.r, op.s);
         // We purposely ignore success and return values since the BatchExecutor will most likely be called by an EOA
-        op.account.call{gas: op.gasLimit}(data);
+        // Lower-level call is used to avoid reverting on failure
+        (bool success, bytes memory retData) = op.account.call{gas: op.gasLimit}(data);
+        return (success, retData);
     }
 }
